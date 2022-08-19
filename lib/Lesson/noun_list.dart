f5d51e73fbfc0c0d569ddf5b080c05c969623d69
CI-CD <bsse1003@iit.du.ac.bd>
2022-08-19 12:30:40 +0600

    work in progress...

--- lib/Lesson/noun_list.dart
+++ lib/Lesson/noun_list.dart
@@ -1,6 +1,10 @@
 //read from folder /files and store here
 //array of title, meaning, imageList, audio
 //start.....
+import 'dart:io';
+
+import 'package:student/globals.dart' as globals;
+
 class NounList {
   String title = '';
   String meaning = '';
@@ -16,9 +20,35 @@ class NounList {
     List<String> values = line.split("; ");
     title = values[0];
     meaning = values[1];
-    imagePath = getImagePaths(values[2].split('/').last);
-    audio = values[3].split('/').last;
+
+    getAudioPath(values[3].split('/').last);
+
+    getImagePaths(values[2].split('/').last).then((data) {
+      imagePath = data;
+    });
   }
 
-  getImagePaths(String folderName) {}
+  Future getImagePaths(String folderName) async {
+    String folderPath = '${globals.folderPath}/Lesson/Noun/$folderName';
+    //Future listDir(String folderPath) async {
+    var directory = Directory(folderPath);
+    //print(directory);
+
+    var exists = await directory.exists();
+    if (exists) {
+      directory
+          .list(recursive: true, followLinks: false)
+          .listen((FileSystemEntity entity) {
+        String path = entity.path.replaceAll('\\', '/');
+        imagePath.add(path);
+      });
+    }
+
+    return imagePath;
+    //}
+  }
+
+  void getAudioPath(String file) {
+    audio = '${globals.folderPath}/Lesson/Noun/$file';
+  }
 }
